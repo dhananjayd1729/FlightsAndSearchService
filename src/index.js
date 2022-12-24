95a753beb7786a942847dfc4a705b9b1c5af90e2
Dhananjay <dhananjay@pop-os.localdomain>
2022-12-24 22:16:31 +0530

    setup body-parser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 //cjs moduling
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
   //create express object
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   const PORT = 3000;
 
   app.listen(PORT, () => {
