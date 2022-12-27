6d859e4861e4ef0352aab3b23a2a478db9f5d843
Dhananjay <dhananjay@pop-os.localdomain>
2022-12-27 17:35:35 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,20 +3,50 @@ const City = require("../models/index");
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = City.create({
+        name,
+      });
       return city;
     } catch (error) {
+      console.log("Something went wrong in repository layer.");
       throw { error };
     }
   }
-  async deleteCity({ name }) {
+
+  async deleteCity(cityId) {
     try {
       await City.destroy({
         where: {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log("Something went wrong in repository layer.");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = City.findbyPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in repository layer.");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in repository layer.");
       throw { error };
     }
   }
