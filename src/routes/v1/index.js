3e15bfcc5c72532913a983fd13f775cb06f8c342
Dhananjay <dhananjay@pop-os.localdomain>
2022-12-31 00:11:12 +0530

    Added flights search crud with few filters

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -11,5 +11,6 @@ router.patch("/city/:id", CityController.update);
 router.get("/city", CityController.getCities);
 
 router.post("/flights", FlightController.create);
+router.get("/flights", FlightController.getAll);
 
 module.exports = router;
